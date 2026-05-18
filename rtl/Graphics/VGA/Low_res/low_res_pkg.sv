5191edb2ec7a7e1e8ad4015df752858b777881a1
AdamAntoniKrupa <adamantonikrupa@gmail.com>
2026-05-18 19:53:32 +0200

    feat: low-res interface and package, LUT to VGA color format converter; refactor: moved vga files to folders with corresponding resolution

---
+++ rtl/Graphics/VGA/Low_res/low_res_pkg.sv
@@ -0,0 +1,7 @@
+package low_res_pkg;
+
+    // Parameters for the resolution that will be upscaled at the end
+    localparam LOW_RES_HOR_PIXELS = 256;
+    localparam LOW_RES_VER_PIXELS = 192;
+
+endpackage
