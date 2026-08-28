1d6b0335216d3ef151ecb7626748a641b2baed8c
MelonMusk64 <j.krzysztof13@gmail.com>
2026-08-29 00:58:02 +0200

    The FPGA programming script has been modified to work when more than one board is connected

--- fpga/scripts/program_fpga.tcl
+++ fpga/scripts/program_fpga.tcl
@@ -3,13 +3,10 @@
 # Author: Piotr Kaczmarczyk
 #
 # Description:
-# tcl file sourced to Vivado to load the bitstream specified in the argument to an FPGA.
-
-
+# tcl file sourced to Vivado to load the bitstream specified in the argument to ALL connected FPGAs sequentially.
 
 # Bitstream location
 set bitstream_file [lindex ${argv} 0]
-
 
 # Load bitstream to FPGA
 proc program_fpga {bitstream_file} {
@@ -19,24 +16,47 @@ proc program_fpga {bitstream_file} {
     } else {
         open_hw_manager
         connect_hw_server
-        current_hw_target [get_hw_targets *]
-        open_hw_target
-        current_hw_device [lindex [get_hw_devices] 0]
-        refresh_hw_device -update_hw_probes false [lindex [get_hw_devices] 0]
-
-        set_property PROBES.FILE {} [lindex [get_hw_devices] 0]
-        set_property FULL_PROBES.FILE {} [lindex [get_hw_devices] 0]
-        set_property PROGRAM.FILE ${bitstream_file} [lindex [get_hw_devices] 0]
-
-        program_hw_devices [lindex [get_hw_devices] 0]
-        refresh_hw_device [lindex [get_hw_devices] 0]
+        
+        # Get a list of all connected hardware targets
+        set targets [get_hw_targets]
+        
+        if {[llength $targets] == 0} {
+            puts "ERROR: No hardware targets connected!"
+            exit 1
+        }
+        
+        # Iterate through all discovered boards
+        foreach target $targets {
+            puts "========================================"
+            puts " Programming target: $target"
+            puts "========================================"
+            
+            current_hw_target $target
+            open_hw_target
+            
+            set device [lindex [get_hw_devices] 0]
+            current_hw_device $device
+            refresh_hw_device -update_hw_probes false $device
+            
+            set_property PROBES.FILE {} $device
+            set_property FULL_PROBES.FILE {} $device
+            set_property PROGRAM.FILE ${bitstream_file} $device
+            
+            program_hw_devices $device
+            refresh_hw_device $device
+            
+            # Close the current target before moving to the next one
+            close_hw_target
+        }
+        
+        disconnect_hw_server
+        close_hw_manager
     }
 }
 
-
 ## MAIN
 if {${argc} != 1} {
-    puts "ERROR: Bitsream not specified"
+    puts "ERROR: Bitstream not specified"
     exit 1
 } else {
     program_fpga $bitstream_file
